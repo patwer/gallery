976655a002498eb9e1a0e99f7b4f3ea3f4b36bf7
Patwer <patweru@gmail.com>
2023-03-12 18:36:46 +0300

    updated pass and user

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://patweru:12wsx#EDC$@cluster0.eubdwv8.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://patweru:12wsx#EDC$@cluster0.eubdwv8.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://patweru:12wsx#EDC$@cluster0.eubdwv8.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
