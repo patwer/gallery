8c856bd0dc877b798afdf290929b585a2376e31b
Patwer <patweru@gmail.com>
2023-03-13 20:24:30 +0300

    replace localhost

--- server.js
+++ server.js
@@ -42,5 +42,5 @@ app.use('/image', image);
  
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
+    console.log(`Server is listening at http://127.0.0.1:${PORT}`)
 });
