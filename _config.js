ba842c027d89fcea46f03da5e05899c739840649
Patwer <patweru@gmail.com>
2023-03-13 21:52:43 +0300

    update password

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://patweru:12wsx#EDC$@cluster0.eubdwv8.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://patweru:12wsx#EDC$@cluster0.eubdwv8.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://patweru:12wsx#EDC$@cluster0.eubdwv8.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://patweru:12wsx3edc4@cluster0.eubdwv8.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://patweru:12wsx3edc4@cluster0.eubdwv8.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://patweru:12wsx3edc4@cluster0.eubdwv8.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
